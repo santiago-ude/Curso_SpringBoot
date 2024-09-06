d6969fe4d7606acaf99696988dc8e6bdde9dff1f
santiago ude <107772887+santiago-ude@users.noreply.github.com>
2024-09-06 09:31:52 -0300

    Add files via upload

---
+++ PrimeraPrueba/src/test/java/com/PruebaSpringBoot/Pruebita/PruebitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaSpringBoot.Pruebita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
