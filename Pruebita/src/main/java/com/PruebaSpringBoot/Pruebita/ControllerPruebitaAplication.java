2b31e69c0346c9d2508d5958657739fc806e65a8
santiago ude <107772887+santiago-ude@users.noreply.github.com>
2024-09-06 22:13:45 -0300

    Update ControllerPruebitaAplication.java

--- Pruebita/src/main/java/com/PruebaSpringBoot/Pruebita/ControllerPruebitaAplication.java
+++ Pruebita/src/main/java/com/PruebaSpringBoot/Pruebita/ControllerPruebitaAplication.java
@@ -1,22 +1,31 @@
 package com.PruebaSpringBoot.Pruebita;
 
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController // Estoy diciendo que esto va a porcesar solicitudes HTTP
 //@Controller esto sirve para el holaMundo que me renderiza el codigo
-public class ControllerPruebitaAplication {
+public class ControllerSpringBoot {
 
-    @GetMapping("/hola")  // cada uno de estos metodos es un endpoint
-    public String HolaMundo() {
-        return "Hola mundo desde SpringBoot!";
+    @GetMapping("/hola")// cada uno de estos metodos es un endpoint
+    public String hola(){
+        return String.format("Hola");
     }
 
-    @GetMapping("/holaMundo") // si yo pongo /holaMundo me va a ejecutar lo que este dentro de este metodo
-    public String holaMundo() {
-        return "hola Es una segunda prueba";
+
+    @GetMapping("/descripcion")// si yo pongo /descripcion me va a ejecutar lo que este dentro de este metodo
+    public String holaMundo(Model model){
+
+        model.addAttribute("nombre","santiago");
+        model.addAttribute("apellido","ude");
+        model.addAttribute("edad",20);
+        return String.format("Hola, mi nombre es " + model.getAttribute("nombre") + " " + model.getAttribute("apellido" )+ " " +
+        "y tengo " + model.getAttribute("edad") + " anios");
     }
+
 
 
 }
